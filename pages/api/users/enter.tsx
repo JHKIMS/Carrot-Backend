79127f2236e9c8fe288e17efc263ec7099b4be5c
JHKIMS <wjdgns5131@gmail.com>
2023-08-10 07:42:34 +0900

    Carrot-Auth: Twilio을 활용한 인증 구현

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,7 +1,9 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import withHandler, { ResponseType } from "../../../libs/server/withHandler";
 import client from "../../../libs/server/client";
+import twilio from "twilio";
 
+const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
 async function handler(
   req: NextApiRequest,
@@ -9,8 +11,8 @@ async function handler(
 ) {
   const { phone, email } = req.body;
   const uniqueKey = Math.floor(100000 + Math.random() * 900000) + "";
-  const user = phone ? { phone: +phone } : email ? { email }:null;
-  if(!user) return res.status(400).json({ok:false});
+  const user = phone ? { phone: +phone } : email ? { email } : null;
+  if (!user) return res.status(400).json({ ok: false });
   const token = await client.token.create({
     data: {
       uniqueKey: uniqueKey,
@@ -27,6 +29,14 @@ async function handler(
       },
     },
   });
+  if (phone) {
+    const message = await twilioClient.messages.create({
+      messagingServiceSid: process.env.TWILIO_SERVICE,
+      to: process.env.MY_PHONE!,
+      body: `Your login token is ${uniqueKey}`,
+    });
+    console.log(message);
+  }
   console.log(token);
   return res.json({
     ok: true,
