02d0a14750729bdef099ab58bf4221b918a3bed2
JHKIMS <wjdgns5131@gmail.com>
2023-08-10 08:03:51 +0900

    Carrot-Auth: SendGrid 활용한 이메일 인증 구현

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,7 +1,10 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import withHandler, { ResponseType } from "../../../libs/server/withHandler";
 import client from "../../../libs/server/client";
-import twilio from "twilio";
+import twilio from "twilio"; 
+import mail from "@sendgrid/mail";
+
+mail.setApiKey(process.env.SENDGRID_APIKEY!);
 
 const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
@@ -36,6 +39,15 @@ async function handler(
       body: `Your login token is ${uniqueKey}`,
     });
     console.log(message);
+  }else if(email){
+    const email = await mail.send({
+      from:"maf12@naver.com",
+      to:"wjdgns5131@gmail.com",
+      subject: "SideProject Vertification Email",
+      text:`Your token ${uniqueKey}`,
+      html: `<strong>Your Token is ${uniqueKey}</strong>`,
+    });
+    console.log(email);
   }
   console.log(token);
   return res.json({
