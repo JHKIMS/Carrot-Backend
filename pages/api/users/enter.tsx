f33a10a01bf68331e1a303540270a984482101f6
JHKIMS <wjdgns5131@gmail.com>
2023-08-09 10:20:47 +0900

    Carrot-Logic: Account Logic

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,8 +1,47 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import withHandler from "../../../libs/server/withHandler";
+import client from "../../../libs/server/client";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
+  const { phone, email } = req.body;
+  let user;
+  if (email) {
+    user = await client.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+    if (user) console.log("found it User.");
+    if (!user) {
+      console.log("Did not find user");
+      user = await client.user.create({
+        data: {
+          name: "Tester",
+          email: email,
+        },
+      });
+    }
+  }
+
+  if (phone) {
+    user = await client.user.findUnique({
+      where: {
+        phone: +phone,
+      },
+    });
+    if (user) console.log("found it User.");
+    if (!user) {
+      console.log("Did not find user");
+      user = await client.user.create({
+        data: {
+          name: "Tester",
+          phone: +phone,
+        },
+      });
+    }
+  }
+  console.log(user);
+
   return res.status(200).end();
 }
 
