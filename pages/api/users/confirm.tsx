f1aef30f723e4a19e07e43d846db1f706668ed00
JHKIMS <wjdgns5131@gmail.com>
2023-08-12 23:40:41 +0900

    Carrot: Iron-Session을 이용해서 유저에게 쿠키제공

--- pages/api/users/confirm.tsx
+++ pages/api/users/confirm.tsx
@@ -1,3 +1,4 @@
+import {withIronSessionApiRoute} from "iron-session/next";
 import { NextApiRequest, NextApiResponse } from "next";
 import withHandler, { ResponseType } from "../../../libs/server/withHandler";
 import client from "../../../libs/server/client";
@@ -12,10 +13,26 @@ async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
-  const { token } = req.body;
+  const {token} = req.body;
+  const exists = await client.token.findUnique({
+    where:{
+      uniqueKey: token,
+    },
+  });
+  console.log("세션값");
+  console.log(req.session);
   console.log("Confrim파일_Token");
   console.log(token);
+  if(!exists) res.status(404).end();
+  console.log(exists);
+  req.session.user = {
+    id: exists?.userId
+  }
+  await req.session.save();
   res.status(200).end();
 }
 
-export default withHandler("POST", handler);
+export default withIronSessionApiRoute(withHandler("POST", handler),{
+    cookieName: "ironTest",
+    password: "1234556766572askgsj1251251255125421dlhkgjeoigsakjjs"
+});
